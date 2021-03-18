65c28ff2167eda2f7b5298eb3c9f96057ddd4b80
nezih <nezih-aksu@hotmail.com>
2021-03-18 17:43:59 +0300

    train_test_split functiona and updated preprocess_pipeline.

--- airbnb_eda.py
+++ airbnb_eda.py
@@ -9,27 +9,32 @@ IMPUTE = True
 INPLACE = True
 ALLOWED_NAN_PER = 10
 DROP_KEYWORDS = ["code","zipcode","link","url","id","name","thumbnail","picture","pic","description","note"]
-NONE_VALUES = [np.nan,None,"None","Null","NONE","NULL","none","null","nan",""," ",0]
+NONE_VALUES = [np.nan,None,"None","Null","NONE","NULL","none","null","nan",""," "]
 OUTLIER_COLUMN = None
+POLYTRANS_COLUMNS = None
 UPPER_QUANTILE = 0.99
 LOWER_QUANTILE = 0.01
 ALLOWED_CORR_PER = 0.8
-
-
+TEST_SIZE = 0.25
+VALIDATION_DATASET = False
 
 class Pipelines():
 
-	def explorer(self,df,file_type=None,x=None,y=None):
-		explorer = Explore(df,file_type)
+	def __init__(self,df,file_type=None):
+		self.df = df
+		self.file_type = file_type
+
+	def explorer(self,x=None,y=None):
+		explorer = Explore(self.df,self.file_type)
 		explorer.intro()
 		explorer.unique_values()
 		explorer.missing_values()
 		explorer.dtype_histogram()
 		explorer.scatter_plot(x,y)
 
-	def cleaner_pipeline(self,df,file_type=None,inplace=None,missing_percentage=10):
-		cleaner = Cleaner(df,file_type)
-		cleaner.drop_column_contains(DROP_KEYWORDS)
+	def cleaner_pipeline(self,drop_keywords,inplace=None,missing_percentage=10):
+		cleaner = Cleaner(self.df,self.file_type)
+		cleaner.drop_column_contains(drop_keywords)
 		cleaner.drop_sentence_columns(inplace=inplace)
 		cleaner.drop_date_columns(inplace=inplace)
 		cleaner.drop_missing_columns(missing_percentage)
@@ -38,21 +43,25 @@ class Pipelines():
 		df = cleaner.space_to_underscore()
 		return df
 	
-	def preprocess_pipeline(self,df,upper_quantile,lower_quantile,outlier_column:str=None,corr_percentage=0.7):
+	def preprocess_pipeline(self,df,upper_quantile,lower_quantile,outlier_column:str=None,polytrans_columns:list=None,corr_percentage=0.7,test_size=0.25,validation=False):
 		preprocess = Preprocess(df)
+		target = preprocess.features_target(df)
 		if outlier_column != None:
 			preprocess.drop_outliers(outlier_column,upper_quantile,lower_quantile)
 		preprocess.drop_multicoll_columns(ALLOWED_CORR_PER)
 		preprocess.imputer()
-		df = preprocess.one_hot_encoder()
-		return df
-
-
+		if polytrans_columns != None:
+			preprocess.polytrans(polytrans_columns)
+		features = preprocess.one_hot_encoder()
+		if validation:
+			x_train,y_train,x_test,y_test,x_validation,y_validation = train_test_split(features,target,test_size,validation)
+		x_train,y_train,x_test,y_test = preprocess.train_test_split(features,target,test_size,validation)
+		return x_train,y_train,x_test,y_test
 
 if __name__ == '__main__':
-	pipelines = Pipelines()
-	expore = Pipelines().explorer(DF,FILE_TYPE)
-	cleaned_df = pipelines.cleaner_pipeline(DF,FILE_TYPE,INPLACE,ALLOWED_NAN_PER)
-	preprocessed_df = pipelines.preprocess_pipeline(cleaned_df,OUTLIER_COLUMN,ALLOWED_CORR_PER)
+	pipelines = Pipelines(DF,FILE_TYPE)
+	expore = Pipelines().explorer()
+	cleaned_df = pipelines.cleaner_pipeline(DROP_KEYWORDS,INPLACE,ALLOWED_NAN_PER)
+	preprocessed_df = pipelines.preprocess_pipeline(cleaned_df,OUTLIER_COLUMN,OUTLIER_COLUMN,POLYTRANS_COLUMNS,ALLOWED_CORR_PER)
 	print(preprocessed_df)
 	
--- preprocess.py
+++ preprocess.py
@@ -5,13 +5,14 @@ from sklearn.pipeline import Pipeline
 from sklearn.preprocessing import OneHotEncoder,PolynomialFeatures
 from sklearn.impute import SimpleImputer
 from sklearn.model_selection import train_test_split
+SEED = 42
 
 class Preprocess():
   """Preprocess the dataset after cleaning."""
   def __init__(self,df):
     self.df = df
     self.categorical_features,self.numerical_features = self._cat_num_features()
-      
+
   def __call__(self):
     return self.df
   
@@ -24,6 +25,13 @@ class Preprocess():
       else:
         discrete_features.append(column)
     return discrete_features,continuous_features
+
+  def features_target(self,target_name:str=None,none_values):
+    self.df = self.df[self.df[target_name] is not in none_values]
+    target = self.df[target_name]
+    self.df = self.df.drop(target_name,axis=1,inplace=True)
+    self.categorical_features,self.numerical_features = self._cat_num_features()
+    return target
 
   def drop_multicoll_columns(self,allowed_corr_percentage:int):
     corr_matrix = self.df[self.numerical_features].corr()
@@ -52,11 +60,10 @@ class Preprocess():
     self.df = pd.concat([encoded_categorical_df,self.df[self.numerical_features]],axis=1)
     return self.df
 
-  def polytrans(self,column):
+  def polytrans(self,columns):
     poly_transformer = PolynomialFeatures(degree=2, interaction_only=True, include_bias=False)
-    self.df[column] = poly_transformer.fit_transform(self.df[column])
+    self.df[columns] = poly_transformer.fit_transform(self.df[columns])
     return self.df
-
 
   #If the size of dataset is less than 1000 outlier's effect can be seen in the model's outcome.
   def drop_outliers(self,column:str,upper_quantile:float=0.99,lower_quantile:float=0.01):
@@ -65,5 +72,12 @@ class Preprocess():
     self.categorical_features,self.numerical_features = self._cat_num_features()
     return self.df
 
-  def train_test_split(self,x,y,validation=False):
-    pass
+  def train_test_split(self,x,y,test_size,validation=False):
+    x_train,y_train,x_test,y_test = train_test_split(x,y,test_size=test_size,random_state=SEED)
+    if validation:
+      x_test,y_test,x_validation,y_validation = train_test_split(x_test,y_test,test_size=test_size,random_state=SEED)
+      return x_train,y_train,x_test,y_test,x_validation,y_validation
+    return x_train,y_train,x_test,y_test    
+
+
+
