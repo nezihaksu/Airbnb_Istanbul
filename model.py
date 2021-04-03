c33c414b170003b8ed33b786ae8591ea0dfa7db6
nezih <nezih-aksu@hotmail.com>
2021-04-04 00:07:34 +0300

    BaseRegression and LinearRegression classes are created.

--- model.py
+++ model.py
@@ -12,93 +12,50 @@ from statistics import Statistics
 la = LinearAlgebra()
 stats = Statistics()
 
-np.random.seed(0)
-np.set_printoptions(precision=1)
 
-y = np.random.randn(100,1)
-x = np.random.randn(100,2)
-
-class StochasticGradientDescent:
-	
-	def __init__(self,lr):
-		self.lr = lr
+class BaseRegression:
+    
+	def __init__(self, learning_rate=0.001, n_iters=1000):
+		self.lr = learning_rate
+		self.n_iters = n_iters
 		self.weights = None
 		self.bias = None
 
-	def in_random_order(self,data):
-		indexes = [i for i,_ in enumerate(data)]
-		random.shuffle(indexes)
+	def fit(self, X, y):
+		n_samples, n_features = X.shape
 
-		for i in indexes:
-			yield data[i]
+        # init parameters
+		self.weights = np.zeros(n_features)
+		self.bias = 0
 
-	def gradient(self,x_i,y_i,y_predicted)		
-		dw = (1 / n_samples) * np.dot(x_i.T, (y_predicted - y_i))
-		db = (1 / n_samples) * np.sum(y_predicted - y_i)
-		return dw,db
+        # gradient descent
+		for _ in range(self.n_iters):
+			y_predicted = self._approximation(X, self.weights, self.bias)
+			residual = np.array(la.vector_subtract(y_predicted,y))
 
-	def stochastic_descent(self,target_fn,gradient_fn,x,y):
-		data = np.array(list(zip(x,y)))
+			# compute gradients
+			dw = (1 / n_samples) * np.dot(X.T,residual)
+			dw_list.append(dw)
+			db = (1 / n_samples) * np.sum(residual)
+			db_list.append(db)
 
-		for x_i,y_i in self.in_random_order(data):
-			y_predicted = target_fn(x_i, self.weights, self.bias)
-			dw,db = self.gradient(x_i,y_i,y_predicted)
-
-			self.weights -= self.lr * dw
+			# update parameters
+			self.weights = la.vector_subtract(self.weights,(self.lr * dw))
 			self.bias -= self.lr * db
-
-	def negate(self,f):
-		return lambda *args,**kwargs:-f(*args,**kwargs)
-	
-	def negate_all(self,f):
-		return lambda *args,**kwargs:[-y for y in f(*args,**kwargs)]
-	
-	def maximize_stochastic(self,target_fn,gradient_fn,x,y):
-		return self.minimize_stochastic(self.negate(target_fn),
-										self.negate_all(gradient_fn),
-										x,y)
 
 	def predict(self, X):
 		return self._predict(X, self.weights, self.bias)
 
 	def _predict(self, X, w, b):
-	    raise NotImplementedError()
-
-	def _approximation(self, X, w, b):
-	    raise NotImplementedError()
-
-class BaseRegression(StochasticGradientDescent):
-
-	def __init__(self):
-		super.__init__()
-	
-	def error(self,a,b,x_i,y_i):
-		return np.subtract(y_i,self._predict(a,b,x_i))
-	
-	def sum_of_squared_errors(self,a,b,x,y):
-		return np.sum(self.error(a,b,x_i,y_i)**2 for x_i,y_i in zip(x,y))
-	
-	def total_sum_of_squares(self,y):
-		np.sum([v**2 for v in stats.de_mean(y)])
-	
-	def r_squared(self,alpha,beta,x,y):
-		return 1.0 - (np.divide(self.sum_of_squared_errors(alpha,beta,x,y),self.total_sum_of_squares(y)))
-	
-	def _predict(self, X, w, b):
-	    raise NotImplementedError()
-
-	def _approximation(self,a,b,x_i):
 		raise NotImplementedError()
 
+	def _approximation(self, X, w, b):
+		raise NotImplementedError()
 
-class MultipleRegression(BaseRegression):
-		
-    def _approximation(self, X, w, b):
-        return np.dot(X, w) + b
+class LinearRegression(BaseRegression):
 
-    def _predict(self, X, w, b):
-        return np.dot(X, w) + b
+	def _approximation(self, X, w, b):
+		return np.dot(X, w) + b
 
-import numpy as np
-
-
+	def _predict(self, X, w, b):
+		return np.dot(X, w) + b
